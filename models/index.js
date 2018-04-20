2d3d798033c3bfbb1f036420948090df3e164826
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-19 20:59:46 -0600

    added process.env

--- models/index.js
+++ models/index.js
@@ -1,3 +1,8 @@
 var mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost/todo-app-demo');
 module.exports.Todo = require("./todo.js");
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL);
+} else {
+  mongoose.connect("mongodb://localhost/whenpresident");
+}
